cc4eda4ef34de3b0b5b6cfc56a759cd06f9c908a
Milo Weinberg <38477514+Iapetus-11@users.noreply.github.com>
2022-12-16 08:46:14 -0500

    Add embeds in apps like Discord/Twitter/Facebook (#2)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,8 @@
+const villagerBotDescription =
+  'Economy, utility, fun, Minecraft, and MORE! You name it? We got it! We have multi-language support too! Español! Français, Português!'
+const villagerBotUrl = 'https://villagerbot.com/'
+const villagerBotIconUrl = 'https://villagerbot.com/villager-bot.webp'
+
 export default defineNuxtConfig({
   target: 'static',
   postcss: {
@@ -12,6 +17,20 @@ export default defineNuxtConfig({
     head: {
       title: 'Villager Bot',
       link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+      meta: [
+        /* meta for embeds in Discord or Facebook */
+        { property: 'og:type', content: 'website' },
+        { property: 'og:title', content: 'Villager Bot' },
+        { property: 'og:description', content: villagerBotDescription },
+        { property: 'og:url', content: villagerBotUrl },
+        { property: 'og:image', content: villagerBotIconUrl },
+        /* meta for embeds in twitter */
+        { name: 'twitter:card', content: 'summary' },
+        { name: 'twitter:title', content: 'Villager Bot' },
+        { name: 'twitter:description', content: villagerBotDescription },
+        { name: 'twitter:url', content: villagerBotUrl },
+        { name: 'twitter:image', content: villagerBotIconUrl },
+      ],
     },
   },
 })
